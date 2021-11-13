8b0849883db1df7762961e6800ccb33c104b306b
alcetasa <stne95@gmail.com>
2021-11-13 12:21:05 -0500

    Tema 2: Completado Introduccion al pipe y transformacio..

--- 02 Introduccion al pipe y transformacion de variables.R
+++ 02 Introduccion al pipe y transformacion de variables.R
@@ -12,25 +12,26 @@ glimpse(data)
 
 ## Pero primero debemos hablar del PIPE %>% 
 
-data <- data %>% 
-  mutate(cyl = as_factor(cyl)) 
+data <- data %>%
+  mutate(cyl = as.factor(cyl)) 
 
 ## Cambiar varias variables al tiempo
 
 data %>% 
-  mutate_at(c("disp", "gear"), as_factor) %>% 
+  mutate_at(c("disp","gear"), as.factor)%>% 
   glimpse()
+
 
 ## Cambiar las variables que cumplan una condición
 
 data %>% 
-  mutate_if(is_double, as_factor) %>% 
+  mutate_if(is_double, as.factor) %>% 
   glimpse()
 
 ## Cambiar todas las variables al tiempo
 
 data %>% 
-  mutate_all(as_factor) %>% 
+  mutate_all(as.factor) %>% 
   glimpse()
 
 glimpse(data)
@@ -41,5 +42,5 @@ data <- data %>%
   mutate(hp2 = ifelse(hp > 100, "Mayor a 100hp", "Menor o igual a 100hp")) 
 
 glimpse(data)  
-
+?ifelse
 
